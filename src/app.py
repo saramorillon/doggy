1d5cf79585574b39b1005892bd5aa97864dc9fba
Sara Morillon <sara.morillon@gmail.com>
2020-11-11 10:20:13 +0100

    Add login and session

--- src/app.py
+++ src/app.py
@@ -1,8 +1,41 @@
-from flask import Flask, render_template, request
+import logging
+import os
+from functools import wraps
+
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SESSION_TYPE'] = 'filesystem'
+
+sess = Session()
+sess.init_app(app)
 
 
-@app.route('/login/<username>')
-def test_template(username: str):
-    return render_template('login.html', username=username)
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/', methods=["GET"])
+@login_required
+def get_home():
+    return render_template('home.html', user=session['user'])
+
+
+@app.route('/login', methods=["GET"])
+def get_login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=["POST"])
+def post_login():
+    session['user'] = {
+        "username": request.form['username']
+    }
+    return redirect('/')
